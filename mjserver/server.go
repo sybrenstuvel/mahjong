d97bfbe8736ad9f4d4ac3b318b6dccc3e53531c4
Sybren A. Stüvel <sybren@stuvel.eu>
2017-12-11 17:51:30 +0100

    Removed warnings

--- mjserver/server.go
+++ mjserver/server.go
@@ -10,29 +10,29 @@ import (
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
-	router.HandleFunc("/score", ScoreHand)
+	router.HandleFunc("/", index)
+	router.HandleFunc("/score", scoreHand)
 
 	listen := ":8080"
 	log.Println("Listening on", listen)
 	log.Fatal(http.ListenAndServe(listen, router))
 }
 
-func Index(w http.ResponseWriter, r *http.Request) {
+func index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprintln(w, "Welcome!<br>")
 	fmt.Fprintln(w, "<a href='/score'>Score your hand</a>")
 }
 
-func ScoreHand(w http.ResponseWriter, r *http.Request) {
+func scoreHand(w http.ResponseWriter, r *http.Request) {
 	//hand := r.URL.RawQuery
 	score := 1 // score.Score(hand)
 	w.Header().Set("Content-Type", "text/plain")
 	fmt.Fprintln(w, score)
 }
 
-func TodoShow(w http.ResponseWriter, r *http.Request) {
+func todoShow(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	todoId := vars["todoId"]
-	fmt.Fprintln(w, "Todo show:", todoId)
+	todoID := vars["todoId"]
+	fmt.Fprintln(w, "Todo show:", todoID)
 }

e21c591a8d7fc53788ec3e1c6a46ed3c945dfa17
Sybren A. Stüvel <sybren@stuvel.eu>
2017-12-11 17:55:14 +0100

    Server: set up logging and take CLI args

--- mjserver/server.go
+++ mjserver/server.go
@@ -1,14 +1,76 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log"
 	"net/http"
+	"time"
+
+	stdlog "log"
 
 	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 )
 
+const serverVersion = "0.1-dev"
+
+var cliArgs struct {
+	version bool
+	verbose bool
+	debug   bool
+}
+
+func parseCliArgs() {
+	flag.BoolVar(&cliArgs.version, "version", false, "Shows the application version, then exits.")
+	flag.BoolVar(&cliArgs.verbose, "verbose", false, "Enable info-level logging.")
+	flag.BoolVar(&cliArgs.debug, "debug", false, "Enable debug-level logging.")
+	flag.Parse()
+}
+
+func configLogging() {
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: true,
+	})
+
+	// Only log the warning severity or above by default.
+	level := log.WarnLevel
+	if cliArgs.debug {
+		level = log.DebugLevel
+	} else if cliArgs.verbose {
+		level = log.InfoLevel
+	}
+	log.SetLevel(level)
+	stdlog.SetOutput(log.StandardLogger().Writer())
+}
+
+func logStartup() {
+	level := log.GetLevel()
+	defer log.SetLevel(level)
+
+	log.SetLevel(log.InfoLevel)
+	log.WithFields(log.Fields{
+		"version": serverVersion,
+	}).Info("Starting Mahjong Server")
+}
+
 func main() {
+	parseCliArgs()
+	if cliArgs.version {
+		fmt.Println(serverVersion)
+		return
+	}
+
+	configLogging()
+	logStartup()
+
+	// Set some more or less sensible limits & timeouts.
+	http.DefaultTransport = &http.Transport{
+		MaxIdleConns:          100,
+		TLSHandshakeTimeout:   3 * time.Second,
+		IdleConnTimeout:       15 * time.Minute,
+		ResponseHeaderTimeout: 15 * time.Second,
+	}
+
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", index)
 	router.HandleFunc("/score", scoreHand)
